11f2a78929a5d18327727464c70c8f698cf6df16
brinkqiang <brink.qiang@gmail.com>
2019-01-02 21:12:15 +0800

    add

---
+++ install/install.bat
@@ -0,0 +1,6 @@
+pip install curses-2.2-cp37-cp37m-win_amd64.whl
+python -m pip --version
+python -m pip install --upgrade --user psutil bottle requests netifaces pystache py-cpuinfo
+python -m pip freeze
+python setup.py install
+PAUSE
